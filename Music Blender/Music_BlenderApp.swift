f93a7f796526c67f99d391d36c52153311838d6a
ltran39 <ltran39@uic.edu>
2023-02-01 16:12:34 -0600

    Initial Commit

---
+++ Music Blender/Music_BlenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_BlenderApp.swift
+//  Music Blender
+//
+//  Created by Tran Long on 01/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Music_BlenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
